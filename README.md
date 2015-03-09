7680c758fd8064cac6e6919748f0171ec11102f2
Macklin Underdown <macklin@detroitlabs.com>
2015-03-09 14:18:18 -0400

    add push notifications

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 [![Build Status](https://travis-ci.org/Selfconference/selfconf-android.svg?branch=master)](https://travis-ci.org/Selfconference/selfconf-android)
 
+## Setup
+
+- create a `debug.properties` file at the root of the repository
+- add your [Parse](https://parse.com/) project credentials
+
+```
+parseApplicationId=XXXXXX
+parseClientSecret=XXXXXX
+```
+
+- sync with Gradle and build the app
+
 ## Run the tests
 
 ```
--- app/src/main/java/org/selfconference/android/App.java
+++ app/src/main/java/org/selfconference/android/App.java
@@ -3,6 +3,10 @@ package org.selfconference.android;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 import dagger.ObjectGraph;
 import io.fabric.sdk.android.Fabric;
@@ -28,6 +32,17 @@ public class App extends Application {
         if (DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
+        Parse.initialize(this, getString(R.string.parse_application_id), getString(R.string.parse_client_id));
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Timber.d("Successfully subscribed to Parse Push");
+                } else {
+                    Timber.e(e, "Error subscribing to Parse Push");
+                }
+            }
+        });
 
         objectGraph = ObjectGraph.create(new SelfConferenceAppModule(this));
     }
