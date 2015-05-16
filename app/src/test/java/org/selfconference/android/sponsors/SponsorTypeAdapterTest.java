9383224e008ee5333fba1a02b9b6e7f7084b0b50
Macklin Underdown <macklin@detroitlabs.com>
2015-05-16 16:51:12 -0400

    update tests to run consistently on Travis

--- app/src/test/java/org/selfconference/android/sponsors/SponsorTypeAdapterTest.java
+++ app/src/test/java/org/selfconference/android/sponsors/SponsorTypeAdapterTest.java
@@ -5,9 +5,6 @@ import com.google.gson.GsonBuilder;
 
 import org.junit.Test;
 
-import java.io.Reader;
-
-import static org.selfconference.android.FileLoader.loadJson;
 import static org.selfconference.android.sponsors.SponsorAssert.assertThat;
 
 public class SponsorTypeAdapterTest {
@@ -17,8 +14,7 @@ public class SponsorTypeAdapterTest {
 
     @Test
     public void duplicateSponsorLevelsAreRemovedWhenSerializingFromJson() {
-        final Reader reader = loadJson("sponsor-with-multiple-sponsor-levels.json");
-        final Sponsor sponsor = GSON.fromJson(reader, Sponsor.class);
+        final Sponsor sponsor = GSON.fromJson(sponsorWithDuplicateSponsorLevelsJson(), Sponsor.class);
 
         assertThat(sponsor)
                 .hasId(3)
@@ -26,5 +22,51 @@ public class SponsorTypeAdapterTest {
                 .hasLink("http://detroitlabs.com")
                 .hasPhoto("http://s3.amazonaws.com/selfconf/sponsors/dl.png")
                 .hasNumberOfSponsorLevels(3);
+    }
+
+    private static String sponsorWithDuplicateSponsorLevelsJson() {
+        return "{\n" +
+                "  \"id\":3,\n" +
+                "  \"name\":\"Detroit Labs\",\n" +
+                "  \"link\":\"http://detroitlabs.com\",\n" +
+                "  \"photo\":\"http://s3.amazonaws.com/selfconf/sponsors/dl.png\",\n" +
+                "  \"event_id\":1,\n" +
+                "  \"created_at\":\"2015-03-02T14:50:48.570Z\",\n" +
+                "  \"updated_at\":\"2015-03-02T14:50:48.570Z\",\n" +
+                "  \"sponsor_levels\":[\n" +
+                "    {\n" +
+                "      \"id\":7,\n" +
+                "      \"name\":\"Diversity\",\n" +
+                "      \"event_id\":1,\n" +
+                "      \"created_at\":\"2015-03-02T14:50:48.425Z\",\n" +
+                "      \"updated_at\":\"2015-03-02T14:50:48.425Z\",\n" +
+                "      \"order\":7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\":7,\n" +
+                "      \"name\":\"Diversity\",\n" +
+                "      \"event_id\":1,\n" +
+                "      \"created_at\":\"2015-03-02T14:50:48.425Z\",\n" +
+                "      \"updated_at\":\"2015-03-02T14:50:48.425Z\",\n" +
+                "      \"order\":7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\":9,\n" +
+                "      \"name\":\"Lanyard\",\n" +
+                "      \"event_id\":1,\n" +
+                "      \"created_at\":\"2015-03-02T14:50:48.442Z\",\n" +
+                "      \"updated_at\":\"2015-03-02T14:50:48.442Z\",\n" +
+                "      \"order\":9\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\":12,\n" +
+                "      \"name\":\"Snack\",\n" +
+                "      \"event_id\":1,\n" +
+                "      \"created_at\":\"2015-03-02T14:50:48.476Z\",\n" +
+                "      \"updated_at\":\"2015-03-02T14:50:48.476Z\",\n" +
+                "      \"order\":12\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
     }
 }
