641049ff7aa97ae7d6c2d898ca8cf6db6f074dd0
Macklin Underdown <macklin@detroitlabs.com>
2016-04-01 17:20:31 -0400

    Document SponsorComparator

--- app/src/main/java/org/selfconference/android/sponsors/SponsorComparator.java
+++ app/src/main/java/org/selfconference/android/sponsors/SponsorComparator.java
@@ -4,6 +4,17 @@ import com.google.common.collect.ComparisonChain;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * A {@link Comparator} used to compare two {@link Sponsor}s in the following order:
+ *
+ * <ul>
+ *  <li>Orders by lowest {@link SponsorLevel#order()}, which indicates the highest level
+ * of sponsorship. Ex: <strong>Platinum</strong> has an order of 1, <strong>Gold</strong> has an
+ * order of 2, etc.</li>
+ *  <li>If the two sponsors share the lowest common denominator {@link SponsorLevel#order()},
+ *  they are ordered alphabetically using {@link SponsorLevel#name()}.</li>
+ * </ul>
+ */
 final class SponsorComparator implements Comparator<Sponsor> {
   @Override public int compare(Sponsor lhs, Sponsor rhs) {
     SponsorLevel lhsMin = Collections.min(lhs.sponsorLevels());
