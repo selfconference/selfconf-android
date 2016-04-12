bb867ad8edf1dd8479a2f1e53d64ec0525f394b1
Macklin Underdown <macklin@detroitlabs.com>
2016-04-11 23:40:30 -0400

    Add device information debug section

--- app/src/internalDebug/java/org/selfconference/android/ui/debug/DebugView.java
+++ app/src/internalDebug/java/org/selfconference/android/ui/debug/DebugView.java
@@ -1,10 +1,13 @@
 package org.selfconference.android.ui.debug;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.f2prateek.rx.preferences.Preference;
@@ -12,11 +15,19 @@ import javax.inject.Inject;
 import org.selfconference.android.R;
 import org.selfconference.android.data.CaptureIntents;
 import org.selfconference.android.data.Injector;
+import org.selfconference.android.ui.decorator.DisplayMetricsDecorator;
 import timber.log.Timber;
 
 public final class DebugView extends FrameLayout {
 
   @Bind(R.id.debug_capture_intents) Switch captureIntentsView;
+
+  @Bind(R.id.debug_device_make) TextView deviceMakeView;
+  @Bind(R.id.debug_device_model) TextView deviceModelView;
+  @Bind(R.id.debug_device_resolution) TextView deviceResolutionView;
+  @Bind(R.id.debug_device_density) TextView deviceDensityView;
+  @Bind(R.id.debug_device_release) TextView deviceReleaseView;
+  @Bind(R.id.debug_device_api) TextView deviceApiView;
 
   @Inject @CaptureIntents Preference<Boolean> captureIntents;
 
@@ -31,10 +42,31 @@ public final class DebugView extends FrameLayout {
     LayoutInflater.from(context).inflate(R.layout.debug_view_content, this);
     ButterKnife.bind(this);
 
+    setupMockBehaviorSection();
+    setupDeviceSection();
+  }
+
+  private void setupMockBehaviorSection() {
     captureIntentsView.setChecked(captureIntents.get());
     captureIntentsView.setOnCheckedChangeListener((compoundButton, isChecked) -> {
       Timber.d("Capture intents set to %s", isChecked);
       captureIntents.set(isChecked);
     });
+  }
+
+  private void setupDeviceSection() {
+    DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+    DisplayMetricsDecorator decorator = DisplayMetricsDecorator.decorate(displayMetrics);
+
+    deviceMakeView.setText(truncateAt(Build.MANUFACTURER, 20));
+    deviceModelView.setText(truncateAt(Build.MODEL, 20));
+    deviceResolutionView.setText(decorator.resolution());
+    deviceDensityView.setText(decorator.density());
+    deviceReleaseView.setText(Build.VERSION.RELEASE);
+    deviceApiView.setText(String.valueOf(Build.VERSION.SDK_INT));
+  }
+
+  private static String truncateAt(String string, int length) {
+    return string.length() > length ? string.substring(0, length) : string;
   }
 }
---
+++ app/src/internalDebug/java/org/selfconference/android/ui/decorator/DisplayMetricsDecorator.java
@@ -0,0 +1,45 @@
+package org.selfconference.android.ui.decorator;
+
+import android.util.DisplayMetrics;
+
+public final class DisplayMetricsDecorator {
+
+  private final DisplayMetrics displayMetrics;
+
+  public static DisplayMetricsDecorator decorate(DisplayMetrics displayMetrics) {
+    return new DisplayMetricsDecorator(displayMetrics);
+  }
+
+  private DisplayMetricsDecorator(DisplayMetrics displayMetrics) {
+    this.displayMetrics = displayMetrics;
+  }
+
+  public String resolution() {
+    return displayMetrics.heightPixels + "x" + displayMetrics.widthPixels;
+  }
+
+  public String density() {
+    return displayMetrics.densityDpi + "dpi (" + densityBucket() + ")";
+  }
+
+  private String densityBucket() {
+    switch (displayMetrics.densityDpi) {
+      case DisplayMetrics.DENSITY_LOW:
+        return "ldpi";
+      case DisplayMetrics.DENSITY_MEDIUM:
+        return "mdpi";
+      case DisplayMetrics.DENSITY_HIGH:
+        return "hdpi";
+      case DisplayMetrics.DENSITY_XHIGH:
+        return "xhdpi";
+      case DisplayMetrics.DENSITY_XXHIGH:
+        return "xxhdpi";
+      case DisplayMetrics.DENSITY_XXXHIGH:
+        return "xxxhdpi";
+      case DisplayMetrics.DENSITY_TV:
+        return "tvdpi";
+      default:
+        return String.valueOf(displayMetrics.densityDpi);
+    }
+  }
+}
