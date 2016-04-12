9ae39d34654ecf789762eb021c473b1506386a9f
Macklin Underdown <macklin@detroitlabs.com>
2016-04-11 21:34:32 -0400

    Add Telescope for bug reporting

--- app/src/internalDebug/java/org/selfconference/android/ui/DebugViewContainer.java
+++ app/src/internalDebug/java/org/selfconference/android/ui/DebugViewContainer.java
@@ -1,11 +1,15 @@
 package org.selfconference.android.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.mattprecious.telescope.EmailDeviceInfoLens;
+import com.mattprecious.telescope.Lens;
+import com.mattprecious.telescope.TelescopeLayout;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.selfconference.android.R;
@@ -21,12 +25,24 @@ import org.selfconference.android.R;
     ViewHolder viewHolder = new ViewHolder();
     ButterKnife.bind(viewHolder, activity);
 
+    TelescopeLayout.cleanUp(activity);
+    Lens lens = buildLens(activity);
+    viewHolder.telescopeLayout.setLens(lens);
+
     return viewHolder.content;
+  }
+
+  private static Lens buildLens(Context context) {
+    String applicationName = context.getString(R.string.application_name);
+    String subject = context.getString(R.string.bug_report_email_subject, applicationName);
+    String email = context.getString(R.string.bug_report_email_address);
+    return new EmailDeviceInfoLens(context, subject, email);
   }
 
   static final class ViewHolder {
     @Bind(R.id.debug_drawer_layout) DrawerLayout drawerLayout;
-    @Bind(R.id.debug_drawer) ViewGroup debugDrawer;
-    @Bind(R.id.debug_drawer_content) FrameLayout content;
+    @Bind(R.id.telescope_container) TelescopeLayout telescopeLayout;
+    @Bind(R.id.debug_drawer_content) ViewGroup debugDrawer;
+    @Bind(R.id.debug_content) FrameLayout content;
   }
 }
